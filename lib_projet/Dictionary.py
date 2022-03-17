df3a7bdeeabab96f38617aa67fc7f7584661c60c
zackbmz9106 <zakariunibet@gmail.com>
2022-03-18 00:23:11 +0100

    Creating the library + Adding a dictionary

--- lib_projet/Dictionary.py
+++
@@ -1,5 +0,0 @@
-Dictionary = {                      # Definition du dictionnaire 
-        T1  :  [],                  # La tache T1 n'est precedée par aucune tache
-        T2  :  T1,                  # La tache T2 est precedée par la tache 1
-        somme  :  [T1,T2]           # La tache somme n'est prècedée par les taches 1 et 2
-            }
